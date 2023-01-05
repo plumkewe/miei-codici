d8e1a8fd549240a5bd26ebe61231eb035fda18a4
plumpkekes <113628928+plumkewe@users.noreply.github.com>
2023-01-05 16:41:16 +0100

    Bho
    Funziona

---
+++ Miei codici/Algoritmi/Recursion-riemp_stamp_array.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+//  
+
+
+int riempi (int numeri[10], int lunghezza) {
+	
+	numeri [lunghezza] = lunghezza;
+	
+	if (lunghezza == 0)
+		return numeri[lunghezza] = lunghezza;
+	
+	else
+		return riempi (numeri, lunghezza - 1);
+}
+
+void stampa (int numeri[10], int lunghezza, int i) {
+	
+	
+	if (i == lunghezza + 1)
+		return;
+	
+	cout << numeri[i] << " ";
+	i ++;
+	
+	stampa(numeri, lunghezza, i);
+	
+	
+}
+
+
+int main() {
+	setlocale(LC_ALL, "italian");
+	
+	int numeri [10];
+	
+	riempi(numeri, 9);
+	stampa(numeri, 9, 0);
+	
+	
+	return 0;
+}
