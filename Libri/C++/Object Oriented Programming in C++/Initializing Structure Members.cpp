c1d3a03adb9d7c9587fc3fcb78e35371de7a0203
plumpkekes <113628928+plumkewe@users.noreply.github.com>
2023-01-19 19:25:30 +0100

    Up

---
+++ Libri/C++/Object Oriented Programming in C++/Initializing Structure Members.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+//  
+
+struct parte
+{
+	int numeromodello;  //  membri della struttura
+	int   numeroparte;
+	float       costo;
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "italian");
+	
+	parte p1 { 6453, 42, 231.3f };
+	parte p2 ;
+	
+	cout << "Modello: "      << setw(14) << right << p1.numeromodello << endl;
+	cout << "Numero parte: " << setw(7)  << right << p1.numeroparte << endl;
+	cout << "Costo: "        << setw(17) << right << p1.costo << endl;
+	
+	p2 = p1; //  si assegna un struttura ad un'unaltra, solo dello stesso tipo
+	
+	cout << endl;
+	cout << "Modello: "      << setw(14) << right << p2.numeromodello << endl;
+	cout << "Numero parte: " << setw(7)  << right << p2.numeroparte << endl;
+	cout << "Costo: "        << setw(17) << right << p2.costo << endl;
+	
+	return 0;
+}
---
+++ Libri/C++/Object Oriented Programming in C++/Structs.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+//  
+
+struct parte
+{
+	int numeromodello;  //  membri della struttura
+	int   numeroparte;
+	float       costo;
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "italian");
+	
+	parte p1; //  variabile della struttura
+	//  access /dot operator seve per accedere al membro della struttura
+	p1.numeromodello = 6453;
+	p1.numeroparte   = 41;
+	p1.costo         = 432.31f;
+	
+	cout << "Modello: "      << setw(14) << right <<  p1.numeromodello << endl;
+	cout << "Numero parte: " << setw(7) << right << p1.numeroparte << endl;
+	cout << "Costo: "        << setw(18) << right << p1.costo << endl;
+	
+	
+	return 0;
+}
