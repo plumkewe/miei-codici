102233ff904843c698f38b0eed5c40976b6380db
plumpkekes <113628928+plumkewe@users.noreply.github.com>
2023-01-27 15:22:55 +0100

    Inizio classi

---
+++ Libri/C++/Object Oriented Programming in C++/Classes/Simple Class.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+//  
+
+class smallobj  //  defizione della classe
+{
+private:
+	int somedata;  //  class data
+public:
+	void setdada(int d)  //  funzione membro per impostare data
+	{
+		somedata = d;
+	}
+	void showdata()  //  funzione membro per visualizzare data
+	{
+		cout << "Data is " << somedata << endl;
+	}
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "italian");
+	
+	smallobj s1, s2; //  due oggetti di classe smallobj (instance of a class)
+	
+	s1.setdada(1923); //  richiamo di funzione membro per assegnare il valore
+	s2.setdada(1312);
+	
+	s1.showdata(); //  richiamo di funzione per visualizzare il valore
+	s2.showdata();
+	
+	
+	return 0;
+}
