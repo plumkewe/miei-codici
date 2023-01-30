42039b38907e4cce3fd3b40c98a7828bc1a49695
plumpkekes <113628928+plumkewe@users.noreply.github.com>
2023-01-30 20:44:29 +0100

    up

---
+++ Libri/C++/Object Oriented Programming in C++/Classes/Constructor (A Counter Example).cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+//  
+
+class Counter
+{
+private:
+	unsigned int count;
+	
+public:
+	Counter() : count(0)  //  constructor
+	{
+		
+	}
+	void inc_count()
+	{
+		count ++;
+	}
+	int get_count()
+	{
+		return count;
+	}
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "italian");
+	
+	Counter c1, c2;
+	
+	cout << "c1 = " << c1.get_count() << endl;
+	cout << "c2 = " << c2.get_count() << endl;
+	
+	c1.inc_count();
+	c2.inc_count();
+	c2.inc_count();
+	
+	cout << "c1 = " << c1.get_count() << endl;
+	cout << "c2 = " << c2.get_count() << endl;
+	
+	
+	return 0;
+}
