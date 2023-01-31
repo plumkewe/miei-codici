b5fecde006c9946899c0c3df8f4d2e30f5203749
plumpkekes <113628928+plumkewe@users.noreply.github.com>
2023-01-31 21:25:02 +0100

    up

---
+++ Libri/C++/Object Oriented Programming in C++/Classes/Objects as Function Arguments.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+//  
+
+class Distance 
+{
+private:
+	int     feet;
+	float inches;
+	
+public:
+	Distance() : feet(0), inches(0.0)
+	{
+	}
+	Distance(int ft, float in) : feet(ft), inches(in)
+	{
+	}
+	
+	void getdist()
+	{
+		cout << "enter feet: ";
+		cin  >>  feet;
+		
+		cout << "Enter inches: ";
+		cin  >>  inches;
+	}
+	
+	void showdist()
+	{
+		cout << feet << "\'-" << inches << '\"';
+	}
+	
+	void add_dist( Distance, Distance );
+};
+
+void Distance::add_dist(Distance d2, Distance d3)
+{
+	inches = d2.inches + d3.inches;
+	feet = 0;
+	
+	if (inches >= 12.0)
+	{
+		inches -= 12.0;
+		feet++;
+	}
+	feet += d2.feet + d3.feet;
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "italian");
+	
+	Distance dist1, dist3;
+	Distance dist2(11, 6.25);
+	
+	dist1.getdist();
+	dist3.add_dist(dist1, dist2);
+	
+	cout << "\ndist1 = ";
+	dist1.showdist();
+	cout << "\ndist2 = ";
+	dist2.showdist();
+	cout << "\ndist3 = ";
+	dist3.showdist();
+	
+	
+	return 0;
+}
