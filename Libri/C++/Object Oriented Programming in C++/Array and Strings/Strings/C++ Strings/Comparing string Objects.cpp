7fd2cef4fd0b877ade0b9ac351050f1c7a614f7c
plumpkekes <113628928+plumkewe@users.noreply.github.com>
2023-02-19 22:38:31 +0100

    up

---
+++ Libri/C++/Object Oriented Programming in C++/Array and Strings/Strings/C++ Strings/Comparing string Objects.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//  
+
+
+int main()
+{
+	setlocale(LC_ALL, "italian");
+	
+	string aName = "Cecilia";
+	string user_name;
+	
+	cout << "Enter you firs name: ";
+	cin  >>  user_name;
+	
+	if (user_name == aName)
+		cout << "Greeatings, Cecilia\n";
+	else if (user_name < aName)
+		cout << "You come before Cecilia\n";
+	else
+		cout << "You come after Cecilia\n";
+	
+	int n = user_name.compare(0, 2, aName, 0, 2);
+	
+	cout << "The first two letters of your name ";
+	
+	if (n == 0)
+		cout << "match ";
+	else if (n < 0)
+		cout << "come before ";
+	else 
+		cout << "come after ";
+	
+	cout << aName.substr(0, 2) << endl;	
+	
+	
+	return 0;
+}
